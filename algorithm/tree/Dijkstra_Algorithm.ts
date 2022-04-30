205d258ec494e5f3efd222029c637d07753a7446
DeltaLF <patricktsaikl@gmail.com>
2022-04-30 20:22:25 +0800

    Dijkstra Algorithm setup

---
+++ algorithm/tree/Dijkstra_Algorithm.ts
@@ -0,0 +1,44 @@
+export {}
+class Node {
+    edges:Edge[] = []
+    visited = false
+    distanceFromStartNode = Infinity
+    previous:Node | null = null
+    
+    constructor(public val:number){}
+
+    addEdges(edge:Edge):void{
+        this.edges.push(edge)
+    }
+    
+}
+
+class Edge{
+    constructor(public node:Node, public weight:number){}
+}
+
+const A = new Node("A");
+const B = new Node("B");
+const C = new Node("C");
+const D = new Node("D");
+const E = new Node("E");
+const F = new Node("F");
+
+A.addEdges(new Edge(B, 2));
+A.addEdges(new Edge(C, 2));
+B.addEdges(new Edge(A, 2));
+B.addEdges(new Edge(D, 1));
+B.addEdges(new Edge(E, 4));
+C.addEdges(new Edge(A, 2));
+C.addEdges(new Edge(D, 1));
+C.addEdges(new Edge(F, 2));
+D.addEdges(new Edge(B, 1));
+D.addEdges(new Edge(C, 1));
+D.addEdges(new Edge(E, 2));
+D.addEdges(new Edge(F, 3));
+E.addEdges(new Edge(B, 4));
+E.addEdges(new Edge(D, 2));
+E.addEdges(new Edge(F, 1));
+F.addEdges(new Edge(C, 2));
+F.addEdges(new Edge(D, 3));
+F.addEdges(new Edge(E, 1));
