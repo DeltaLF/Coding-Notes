09c1b538e1c45314fb8073a5201c42df20ff827d
DeltaLF <patricktsaikl@gmail.com>
2022-05-03 19:47:14 +0800

    lcs test

--- test/dynamical_programming/longestCommonSubsequence.test.ts
+++ test/dynamical_programming/longestCommonSubsequence.test.ts
@@ -1,39 +1,46 @@
-import {lcsRecursion} from "../../algorithm/dynamical_programming/longestCommonSubsequence"
+import {lcsRecursion,lcs} from "../../algorithm/dynamical_programming/longestCommonSubsequence"
 
 
-describe("LCS recursion function",()=>{
-
+function lcsTest(lcs_func:(str1:string,str2:string)=>number){
     test("one empty string",()=>{
         const s1 = ""
         const s2 = "ABC"
-        expect(lcsRecursion(s1,s2)).toBe(0);
+        expect(lcs_func(s1,s2)).toBe(0);
     })
 
     test("tow empty strings",()=>{
         const s1 = ""
         const s2 = ""
-        expect(lcsRecursion(s1,s2)).toBe(0);
+        expect(lcs_func(s1,s2)).toBe(0);
     })
 
     test("s2 is substring of s1",()=>{
         const s1 = "ABCDEFG"
         const s2 = "ABC"
-        expect(lcsRecursion(s1,s2)).toBe(3);
+        expect(lcs_func(s1,s2)).toBe(3);
     })
 
     test("2 identical strings",()=>{
         const s1 = "ABCDEFG"
         const s2 = "ABCDEFG"
-        expect(lcsRecursion(s1,s2)).toBe(7);
+        expect(lcs_func(s1,s2)).toBe(7);
     })
 
     test("random",()=>{
         const s1 = "ANFKSAKASG"
         const s2 = "AKSAKKKKKKKKK"
-        expect(lcsRecursion(s1,s2)).toBe(5);
+        expect(lcs_func(s1,s2)).toBe(5);
     })
+}
 
+describe("LCS recursion function",()=>{
 
+lcsTest(lcsRecursion)
 
+})
+
+describe("LCS dynamical programming",()=>{
+
+    lcsTest(lcs)
 })
 
